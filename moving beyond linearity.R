b6c488b6c5f980ffa19650bf424a69a8ae268045
Marcelo Nunes Alves <marceloalves@flexvision.corp>
2020-09-23 01:56:26 -0300

    Splines/Outras tecnicas

--- moving beyond linearity.R
+++ moving beyond linearity.R
@@ -66,3 +66,64 @@ anova(fit.1, fit.2, fit.3)
 fit=glm(I(wage>250)~poly(age, 4), data=Wage, family=binomial)
 
 preds=predict(fit, newdata = list(age=age.grid), se=T)
+
+pfit=exp(preds$fit)/(1+exp(preds$fit))
+se.bands.logit = cbind(preds$fit+2*preds$se.fit, preds$fit-2*preds$se.fit)
+se.bands = exp(se.bands.logit) / (1+exp(se.bands.logit))
+
+preds = predict(fit, newdata = list(age=age.grid), type="response",se=T)
+
+#Gráfico com as predições
+plot(age, I(wage>250),xlim=agelims,type="n",ylim=c(0,.2))
+points(jitter(age), I((wage>250)/5), cex=.5, pch="|",col="darkgrey")
+lines(age.grid, pfit, lwd=2, col="blue")
+matlines(age.grid,se.bands, lwd=1, col="blue", lty=3)
+
+# Utilização da função cut para a mesma abordagem
+table(cut(age,4))
+fit=lm(wage~cut(age,4),data=Wage)
+coef(summary(fit))
+#Separa os ranges de idade de 33,5, 49 e 64,5 anos, e cria uma regrassão para cada faixa de idade
+
+# Splines
+
+library(splines)
+#Neste ponto é espeficicado as quebras, que geram seis funções diferentes
+fit=lm(wage~bs(age,knots=c(25,40,60)), data=Wage)
+pred=predict(fit,newdata = list(age=age.grid),se=T)
+plot(age,wage,col="gray")
+lines(age.grid, pred$fit,lwd=2)
+lines(age.grid, pred$fit+2*pred$se,lty="dashed")
+lines(age.grid, pred$fit-2*pred$se,lty="dashed")
+
+#Criando spline uniforme atraves das quebra dos quartis 25%, 50% e 75%
+dim(bs(age,knots=c(25,40,60)))
+dim(bs(age,df=6))
+attr(bs(age,df=6), "knots")
+
+#Função ns permiti utilizar 4 grau de liberdade
+fit2=lm(wage~ns(age,df=4),data=Wage)
+pred2=predict(fit2,newdata=list(age=age.grid),se=T)
+lines(age.grid, pred2$fit, col="red", lwd=2)
+
+#Função smoothing spline
+plot(age, wage, xlim=agelims, cex=.5, col="darkgrey")
+title("Smoothing Spline")
+# Selecionando 16 graus de liberdade
+fit=smooth.spline(age,wage,df=16)
+# Selecionando graus de liberdade atraves do cross-validation (Validação cruzada)
+fit2=smooth.spline(age,wage,cv=TRUE)
+fit2$df
+lines(fit, col="red", lwd=2)
+lines(fit2, col="blue", lwd=2)
+legend('topright', legend=c("16 DF", "6.8 DF"), col=c("red", "blue"), lty=1, lwd=2, cex=.8)
+
+# Função local regression
+plot(age,wage,xlim=agelims,cex=.5,col="darkgrey")
+title("Local Regression")
+fit=loess(wage~age,span=.2,data=Wage)
+fit2=loess(wage~age,span=.5,data=Wage)
+lines(age.grid,predict(fit,data.frame(age=age.grid)),col="red",lwd=2)
+lines(age.grid,predict(fit2,data.frame(age=age.grid)),col="blue",lwd=2)
+legend('topright', legend=c("Span=0.2", "Span=0.5"), col=c("red", "blue"), lty=1, lwd=2, cex=.8)
+# O resultado consiste em 20% ou 50% das observações. Quanto maior for a extensão, mais suave será o ajuste
